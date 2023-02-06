3e46f397216f6d2b573ebd1a3b79d43190b09737
unknown <home_dv@mail.ru>
2023-02-06 16:44:58 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = str.split('');
+    let stack = [];
+    let flag = true;
+    brackets.forEach((b) => {
+      let config = bracketsConfig.find((el) => el.includes(b));
+      if(b === config[1] && stack[stack.length - 1] === config[0]) {
+        stack.pop();
+      }
+      else if(b === config[0]){
+        stack.push(b);
+      }
+      else {
+        flag = false;
+      }
+    });
+  return stack.length === 0 && flag;
 }
